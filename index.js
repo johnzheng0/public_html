a27b0914b373bc3f99ba719e0f4f6581a8b140c5
johnzheng0 <jzheng1777@gmail.com>
2022-12-30 00:31:45 -0600

    added scroll effects

--- index.js
+++ index.js
@@ -6,5 +6,18 @@ function loadScript(url)
     script.src = url
     head.appendChild(script)
 }
+
+
+window.addEventListener("scroll", function() {
+    document.body.style.backgroundPositionY = scrollY*0.6 + "px"
+    if (scrollY > 10) {
+        document.getElementById("navbar").style.backgroundColor = "rgba(0, 0, 0, 0.5)"
+        document.getElementById("navbar").style.backdropFilter = "blur(1em)"
+    } else {
+        document.getElementById("navbar").style.backgroundColor = "rgba(0, 0, 0, 0)"
+        document.getElementById("navbar").style.backdropFilter = "blur(0)"
+    }
+})
+
 loadScript("imageCarousel.js")
 loadScript("menubar.js")
