a11d17c575856ac3c8a1f5be919df8f1157f80ed
johnzheng0 <jzheng1777@gmail.com>
2022-12-30 13:48:03 -0600

    fixed some href issues and moved include js code to global

--- about/index.js
+++ about/index.js
@@ -8,28 +8,3 @@ function loadScript(url)
 }
 
 loadScript("../global.js")
-
-function include(element) {
-    let newelem = document.createElement("div")
-    fetch(element.href)
-    .then(res => res.text())
-    .then(text => {
-            newelem.innerHTML = text
-            element.parentNode.replaceChild(newelem,element)
-    let index = Array.from(newelem.parentNode.children).indexOf(newelem)
-    let newelemchildren = newelem.childNodes
-    for (let i=0; i<newelemchildren.length; i++) {
-            newelem.parentNode.insertBefore(newelemchildren[i],newelem)
-    }
-    newelem.parentNode.removeChild(newelem)
-    })
-
-}
-
-window.onload = function() {
-    let includes = document.getElementsByClassName("include")
-    for (let i=0; i<includes.length; i++) {
-            include(includes[i])
-    }
-}
-
--- global.js
+++ global.js
@@ -39,3 +39,29 @@ function eventMenu() {
         }, flyDuration.duration*0.9)
     }
 }
+
+//adds new element to html
+function include(element) {
+    let newelem = document.createElement("div")
+    fetch(element.href)
+    .then(res => res.text())
+    .then(text => {
+            newelem.innerHTML = text
+            element.parentNode.replaceChild(newelem,element)
+    let index = Array.from(newelem.parentNode.children).indexOf(newelem)
+    let newelemchildren = newelem.childNodes
+    for (let i=0; i<newelemchildren.length; i++) {
+            newelem.parentNode.insertBefore(newelemchildren[i],newelem)
+    }
+    newelem.parentNode.removeChild(newelem)
+    })
+
+}
+
+//includes html where include class elements are
+window.onload = function() {
+    let includes = document.getElementsByClassName("include")
+    for (let i=0; i<includes.length; i++) {
+            include(includes[i])
+    }
+}
