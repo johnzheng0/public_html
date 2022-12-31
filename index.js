b29a43fb8b6c8acb95771efeb57726ac23f73e81
johnzheng0 <jzheng1777@gmail.com>
2022-12-31 09:55:17 -0600

    initialize other pages

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function loadScript(url)
 {    
-    var head = document.getElementsByTagName('head')[0]
-    var script = document.createElement('script')
+    let head = document.getElementsByTagName('head')[0]
+    let script = document.createElement('script')
     script.type = 'text/javascript'
     script.src = url
     head.appendChild(script)
