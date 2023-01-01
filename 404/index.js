481357678001eade0e16c1d69a8fd1bc9e02169d
johnzheng0 <jzheng1777@gmail.com>
2023-01-01 10:18:09 -0600

    added 404 page

---
+++ 404/index.js
@@ -0,0 +1,10 @@
+function loadScript(url)
+{    
+    var head = document.getElementsByTagName('head')[0]
+    var script = document.createElement('script')
+    script.type = 'text/javascript'
+    script.src = url
+    head.appendChild(script)
+}
+
+loadScript("../global.js")
